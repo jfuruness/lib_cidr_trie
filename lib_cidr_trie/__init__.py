1517b6962e7dadcc9663def4c399510b8e6d5b25
Justin Furuness <jfuruness@gmail.com>
2023-11-27 02:29:37 -0500

    added PrefixType to __init__.py

--- lib_cidr_trie/__init__.py
+++ lib_cidr_trie/__init__.py
@@ -1,5 +1,5 @@
-from .cidr_trie import CIDRTrie
+from .cidr_trie import CIDRTrie, PrefixType
 from .cidr_tries import IPv4CIDRTrie, IPv6CIDRTrie
 from .cidr_node import CIDRNode
 
-__all__ = ["CIDRTrie", "IPv4CIDRTrie", "IPv6CIDRTrie", "CIDRNode"]
+__all__ = ["CIDRTrie", "IPv4CIDRTrie", "IPv6CIDRTrie", "CIDRNode", "PrefixType"]
